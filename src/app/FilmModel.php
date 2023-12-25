16e66f231d8f4f1cbf27c851368f6baec0112e57
vetsinen <vetsinen@gmail.com>
2023-12-25 16:09:07 +0200

    way to import movies. step 2

--- src/app/FilmModel.php
+++ src/app/FilmModel.php
@@ -4,24 +4,51 @@ namespace Webdev\Filmforge;
 
 class FilmModel
 {
-    public function butchFilmAddingFromTextFile(string $txt) {}
-    public function addFilm() {}
+    public function butchFilmAddingFromTextFile(string $filePath)
+    {
+        error_log('butch importing');
+        $file = fopen($filePath, 'r');
+
+// Check if the file is successfully opened
+        if ($file) {
+            // Loop through each line of the file
+            while (!feof($file)) {
+                $line = fgets($file);
+                echo $line.'<br>'; // Process the line as needed
+            }
+
+            // Close the file handle
+            fclose($file);
+        } else {
+            // Handle the case where the file couldn't be opened
+            echo "Unable to open the file.";
+        }
+    }
+
+    public function addFilm()
+    {
+    }
+
     public function DeleteFilm()
     {
 
     }
+
     public function getList()
     {
 
     }
+
     public function __construct($conn)
     {
 
     }
+
     public function getByTitle()
     {
 
     }
+
     public function getByActor()
     {
 
--- src/upload.php
+++ src/upload.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+require_once(__DIR__ . '/vendor/autoload.php');
+
 if (isset($_SESSION['userid']))
 {echo 'anonymous user can not add movies, please return and login <br>';}
 
@@ -8,11 +10,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the file was uploaded without errors
     if (isset($_FILES["file"]) && $_FILES["file"]["error"] === UPLOAD_ERR_OK) {
         $fileContent = file_get_contents($_FILES["file"]["tmp_name"]);
-        echo "<h1>File Content:</h1>";
-        echo "<pre>" . htmlspecialchars($fileContent) . "</pre>";
+//        echo "<h1>File Content:</h1>";
+//        echo "<pre>" . htmlspecialchars($fileContent) . "</pre>";
 
         //TODO change to Dependendency injection
         $filmModel = new \Webdev\Filmforge\FilmModel(new \Webdev\Filmforge\MySQLConnection());
-
+        $filmModel->butchFilmAddingFromTextFile($_FILES["file"]["tmp_name"]);
+        header("Location: index.php");
     }
 }
